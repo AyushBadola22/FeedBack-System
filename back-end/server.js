2b3ac3642deb93a1265652d12573ba5c235e46f1
AyushBadola22 <ayush.badola.04@gmail.com>
2024-07-13 22:33:04 +0530

    Saving changes to server.js before revert

--- back-end/server.js
+++ back-end/server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 3000;
 const corsOptions = {
     origin : "http://localhost:5173", 
     method : 'GET, POST, PUT, PATCH, HEAD', 
-    credentials : true  
+    credentials : true  ,
 }
 
 app.use(cors(corsOptions));
